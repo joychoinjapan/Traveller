8e02cfc795c63abc2189c699024ac4b49b79f367
miaobaobao <joychoinjapan@gmail.com>
2021-01-18 21:08:55 +0900

    add nav

--- app/src/main/java/com/example/traveller/MainActivity.java
+++ app/src/main/java/com/example/traveller/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.traveller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.traveller.fragment.FindFragment;
+import com.example.traveller.fragment.IndexFragment;
+import com.example.traveller.fragment.MeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.container,new IndexFragment());
+        fragmentTransaction.commit();
+
+    }
+
+
+
+
+    public void myClick(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        switch (view.getId()){
+            case R.id.index:
+                fragmentTransaction.replace(R.id.container,new IndexFragment());
+                break;
+            case R.id.find:
+                fragmentTransaction.replace(R.id.container,new FindFragment());
+                break;
+            case R.id.me:
+                fragmentTransaction.replace(R.id.container,new MeFragment());
+                break;
+        }
+        fragmentTransaction.commit();
     }
 }
